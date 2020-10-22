0b0491915c9919b9368bc1e592231604029eeadc
维他入我心 <t156156@qq.com>
2020-10-22 13:51:41 +0800

    fix import android.util.Log

--- app/src/main/java/com/wtrwx/discuz/MainActivity.java
+++ app/src/main/java/com/wtrwx/discuz/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Lofg;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ValueCallback;
